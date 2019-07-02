87d2fe6336feda43edccfd9628bf30fc23446105
Daydreel <daydreel@gmail.com>
2019-07-02 15:27:55 +0200

    Some Css for starter

--- src/routes/restaurant.svelte
+++ src/routes/restaurant.svelte
@@ -1,29 +1,44 @@
+<style>
+.needs{
+}
+
+.divider{
+  width: 250px;
+  height: 250px;
+  background: orange;
+  border-radius: 50%;
+  align-self: center;
+}
+</style>
+
 <svelte:head>
 	<title>Le restaurant</title>
 </svelte:head>
 
-<h1>Le Restaurant</h1>
+<div class="content">
+  <h1>Le Restaurant</h1>
+  <div class="needs">
+    <h2>Besoins</h2>
+    <p>Besoin de visibilité sur internet</p>
+    <ul>
+      <li>Afficher le menu</li>
+      <li>Chargement rapide sur le Smartphone</li>
+      <li>Contact facile</li>
+      <li>Référencement des moteurs de recherche</li>
+    </ul>
+  </div>
 
-<div>
-  <h2>Besoins</h2>
-  <p>Besoin de visibilité sur internet</p>
-  <ul>
-    <li>Afficher le menu</li>
-    <li>Chargement rapide sur le Smartphone</li>
-    <li>Contact facile</li>
-    <li>Référencement des moteurs de recherche</li>
-  </ul>
-</div>
+  <div class="divider"></div>
 
-
-<div>
-  <h2>Réponses</h2>
-  <p>Catalogue de Site web</p>
-  <ul>
-    <li>Site interne léger</li>
-    <li>Optimisé pour la batterie</li>
-    <li>Conçu pour smartphone et ordinateur</li>
-    <li>De l'UX et Google aime ça</li>
-  </ul>
-  <p><bold>Moins cher pour plus d'efficacité!</bold></p>
+  <div>
+    <h2>Réponses</h2>
+    <p>Catalogue de Site web</p>
+    <ul>
+      <li>Site interne léger</li>
+      <li>Optimisé pour la batterie</li>
+      <li>Conçu pour smartphone et ordinateur</li>
+      <li>De l'UX et Google aime ça</li>
+    </ul>
+    <p><bold>Moins cher pour plus d'efficacité!</bold></p>
+  </div>
 </div>
