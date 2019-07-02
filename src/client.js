b34ddf14d428313ae0cc3b2dd253b562e58870c1
terrygonguet <terry@gonguet.com>
2019-07-02 12:30:03 +0200

    Swipe !!!

--- src/client.js
+++ src/client.js
@@ -1,5 +1,8 @@
 import * as sapper from '@sapper/app';
+import makeSwipe from "./swipe";
 
 sapper.start({
 	target: document.querySelector('#sapper')
+}).then(() => {
+	let swipe = makeSwipe(window)
 });
