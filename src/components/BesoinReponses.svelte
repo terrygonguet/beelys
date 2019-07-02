1045ad4304edbe8c4f259beb9ce2c8aa7fbc98ed
Daydreel <daydreel@gmail.com>
2019-07-02 16:15:51 +0200

    Page Component

--- src/components/BesoinReponses.svelte
+++ src/components/BesoinReponses.svelte
@@ -2,14 +2,20 @@
   export let besoin = true;
   export let title = "Untitled";
   export let items = [];
+  export let punchline;
 </script>
 
 <div>
-  <h2>{besoin ? 'Besoins' : 'Réponses'}</h2>
+  <h2>{besoin ? 'Besoins' : 'Solutions'}</h2>
   <p>{title}</p>
   <ul>
     {#each items as item}
       <li>{item}</li>
     {/each}
   </ul>
+  {#if punchline}
+    <p>
+      <bold>{punchline}</bold>
+    </p>
+  {/if}
 </div>
--- src/routes/restaurant.svelte
+++ src/routes/restaurant.svelte
@@ -2,6 +2,11 @@
   import slide from "../slide";
   import BesRep from "../components/BesoinReponses.svelte";
 
+  //Page
+  let pageTitle = "Le restaurant";
+  let pageBesoin = true;
+
+  //Components
   let title1 = "Besoin de visibilité sur internet";
   let items1 = [
     "Afficher le menu",
@@ -9,28 +14,32 @@
     "Contact facile",
     "Référencement des moteurs de recherche"
   ];
+
+  let title2 = "Catalogue de sites web";
+  let items2 = [
+    "Site interne léger",
+    "Optimisé pour la batterie",
+    "Conçu pour smartphone et ordinateur",
+    "De l'UX et Google aime ça"
+  ];
+  let punchline2 = "Moins cher pour plus d'efficacité!";
 </script>
 
 <svelte:head>
-  <title>Le restaurant</title>
+  <title>{pageTitle}</title>
 </svelte:head>
 
 <main out:slide in:slide={{ fromRight: false }}>
-  <h1>Le Restaurant</h1>
-  <BesRep title={title1} items={items1} />
-  <BesRep title={title2} items={items2} />
-
-  <div>
-    <h2>Réponses</h2>
-    <p>Catalogue de Site web</p>
-    <ul>
-      <li>Site interne léger</li>
-      <li>Optimisé pour la batterie</li>
-      <li>Conçu pour smartphone et ordinateur</li>
-      <li>De l'UX et Google aime ça</li>
-    </ul>
-    <p>
-      <bold>Moins cher pour plus d'efficacité!</bold>
-    </p>
-  </div>
+  <h1>{pageTitle}</h1>
+  {#if pageBesoin}
+    <BesRep title={title1} items={items1} />
+    <button on:click={e => (pageBesoin = false)}>Solutions</button>
+  {:else}
+    <BesRep
+      besoin={false}
+      title={title2}
+      items={items2}
+      punchline={punchline2} />
+    <button on:click={e => (pageBesoin = true)}>Besoins</button>
+  {/if}
 </main>
